a487732fd45efadf2291b1516161f537357558b0
Nadman Faisal <nadmanfaisal@gmail.com>
2025-11-06 17:41:05 +0100

    Add metadata for traceability

--- database/database.py
+++ database/database.py
@@ -5,16 +5,22 @@ import uuid
 from data_cleaning.data_extractor import *
 from embedding.vector_embedding import *
 
+async def delete_collection(collection_name):
+    client = await chromadb.AsyncHttpClient(host='192.168.68.104', port=8000)
+    await client.delete_collection(name=collection_name)
+
 
 async def save_to_db(file_path):
     client = await chromadb.AsyncHttpClient(host='192.168.68.104', port=8000)
     dataset = preprocess_and_chunk(file_path)
     document_embeddings = embed_document(dataset)
     document_ids = []
+    document_metadata = []
 
     for i in range(0, len(dataset)):
         random_id = uuid.uuid4()
         document_ids.append(f'{random_id}')
+        document_metadata.append({"source": file_path})
     
     print(f'Loaded {len(dataset)} coherent chunks')
     print(f'Loaded ids: {len(document_ids)}')
@@ -23,7 +29,8 @@ async def save_to_db(file_path):
     await collection.add(
         documents=dataset,
         ids=document_ids,
-        embeddings=document_embeddings
+        embeddings=document_embeddings,
+        metadatas=document_metadata
     )
     return True
 
